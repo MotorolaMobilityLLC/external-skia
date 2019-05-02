b2919ae395f88650960e885861ba5bfe6fef9d51
Juju Sung <juju.sung@mediatek.com>
2019-05-03 01:20:15 +0800

    [ALPS04325584] [Revert] MTK JPEG decode porting
    1. Porting Android M skia region decode
    2. Region decode multi-thread support
    3. Refine full frame decode to support all kinds of stream
    4. Replace libjpeg-turbo with libjpeg-alpha
    5. MDP feature is not ported yet
    
    Change-Id: I35fcff14ced632f19ffe2345daeccd0de8267879
    CR-Id: ALPS04325584
    Feature: JPEG (Joint Photographic Expert Group) Codec

--- include/codec/SkCodec.h
+++ include/codec/SkCodec.h
@@ -688,11 +688,6 @@ public:
         return this->onGetRepetitionCount();
     }
 
-#ifdef MTK_IMAGE_ENABLE_PQ_FOR_JPEG
-    int getPostProcFlag() const { return fPostProc; }
-    void setPostProcFlag(int flag) { fPostProc = flag;}
-#endif
-
 protected:
     const SkEncodedInfo& getEncodedInfo() const { return fEncodedInfo; }
 
@@ -847,10 +842,6 @@ private:
     int                                fCurrScanline;
 
     bool                               fStartedIncrementalDecode;
-
-#ifdef MTK_IMAGE_ENABLE_PQ_FOR_JPEG
-    int                                fPostProc;
-#endif
 
     bool initializeColorXform(const SkImageInfo& dstInfo, SkEncodedInfo::Alpha, bool srcIsOpaque);
 
